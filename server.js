42b34acdc99ee7117b6e2d16073eb8268408454d
Ben Hawkins <147286158+Hawk-Ben@users.noreply.github.com>
2026-08-27 21:57:38 -0400

    Added more animations

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http'),
       fs   = require('fs'),
-      port = 3000
+  path = require('path'),
+  port = 3000
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
@@ -10,6 +11,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/placidplace-congratulations-7600.gif':
+      sendFile( response, 'placidplace-congratulations-7600.gif', 'image/gif' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
@@ -17,8 +21,15 @@ const server = http.createServer( function( request,response ) {
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function( response, filename ) {
+const sendFile = function( response, filename, contentType = 'text/html' ) {
    fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
+     if (err) {
+       response.statusCode = 404
+       response.end( '404 Error: File Not Found' )
+       return
+     }
+
+     response.setHeader( 'Content-Type', contentType )
+     response.end( content )
    })
 }
